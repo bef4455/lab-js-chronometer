3fe56cc49e48bedc8915c03898f382cfa355fc77
bef4 <befacool@gmail.com>
2023-02-18 11:14:08 +0100

    not solved

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
-
-  getMinutes() {
-    // ... your code goes here
-  }
-
-  getSeconds() {
-    // ... your code goes here
-  }
-
-  getCentiseconds() {
-    // ... your code goes here
-  }
-
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
-
-  stop() {
-    // ... your code goes here
-  }
-
-  reset() {
-    // ... your code goes here
-  }
-
-  split() {
-    // ... your code goes here
-  }
+  // ... your code goes here
 }
+
+start(printTimeCallback) {
+  this.intervalId = setInterval(() => {
+    this.currentTime++
+    if (typeof printTimeCallback === (callback)
+  }, 10);
+}
+// ... your code goes here
+
+
+getMinutes() {
+
+}
+// ... your code goes here
+
+
+getSeconds() {
+
+}
+
+getMilliseconds() {
+
+}
+// ... your code goes here
+
+
+getCentiseconds() {
+  // ... your code goes here
+}
+
+computeTwoDigitNumber(value) {
+}
+// ... your code goes here
+
+
+stop() {
+  // ... your code goes here
+}
+
+reset() {
+  // ... your code goes here
+}
+
+split() {
+  // ... your code goes here
+}
+
--- src/chronometer.js
+++ src/chronometer.js
@@ -1,21 +1,37 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
+
   start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
+
     // ... your code goes here
   }
 
   getMinutes() {
+    const total = Math.floor(this.currentTime % 60)
+    const resultFloor = Math.floor(total)
+    return resultFloor
     // ... your code goes here
   }
 
   getSeconds() {
+    const totalSeconds = this.currentTime % 60
+    return totalSeconds
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+
     // ... your code goes here
   }
 
